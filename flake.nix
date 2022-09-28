2e41a3472f3ebd37e504838846d6703ca7fa5798
bandithedoge <bandithedoge@protonmail.com>
2022-09-28 20:27:25 +0200

    use nix binary cache

--- flake.nix
+++ flake.nix
@@ -1,13 +1,20 @@
 {
+  nixConfig = {
+    substituters = ["https://shine.cachix.org" "https://cache.iog.io"];
+    trusted-public-keys = ["shine.cachix.org-1:nCuHaF+O1/gg0pwwx3z4pIOyqKpAZ8zxwCbSDzgYwh0=" "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+  };
   inputs = {
     haskellNix.url = "github:input-output-hk/haskell.nix";
     nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
   };
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    flake-compat,
     haskellNix,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
